ba05f1a9e2f2a90d63d05696d3fe55f1397f09a4
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-09-30 22:15:55 +0530

    Time: 14 ms (58.93%) | Memory: 19.6 MB (67.83%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,10 @@
-
-#include <algorithm>
-
 class Solution {
 public:
-    int singleNumber(std::vector<int>& nums) {
-        for (auto i = nums.begin(); i != nums.end(); i++) {
-            // Use std::count to count occurrences of *i in the array
-            if (std::count(nums.begin(), nums.end(), *i) == 1) {
-                return *i;  // Return the number itself (not the iterator)
-            }
-        }
-        return 0;  // Return 0 if no single number is found (though this case shouldn't happen as per problem statement)
+    int singleNumber(vector<int>& nums) {
+       int result=0;
+       for(int num : nums ){
+        result^=num;
+       }
+       return result;
     }
 };
