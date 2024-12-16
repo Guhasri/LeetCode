0bebe85e1e65bbc773e1035cab86cbdd9d1da600
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-12-16 10:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (15.96%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int min= *min_element(nums.begin(), nums.end()) ;   
-        int max= *max_element(nums.begin(), nums.end());
-
-        if(min!=0){
-            return 0;
+    int missingNumber(vector<int>& nums) {  
+        int max= nums.size();
+        int req_sum=(max*(max+1))/2;
+        int sum=0;
+        for ( int i=0;i<nums.size();i++){
+           sum+=nums[i];
         }
-
-        for ( int i=1;i<max;i++){
-            auto it = find(nums.begin(), nums.end(), i);
-            if(it == nums.end()){
-                return i;
-            }
-        }
-        return max+1;
+        return (req_sum-sum);
     }
 };
