8bf48c2cf0d8dc18c2658f3691c99dcbd11cfc32
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-12-16 13:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (11.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,35 +3,36 @@ public:
     int maxProfit(vector<int>& prices) {
         int pricesSize=prices.size();
         int profit=0;
-    int minPrice=prices[0];
-    int currentProfit=0;
-    int maxProfit=0;
-    if (pricesSize==2)
-    {
-        if(prices[0]<prices[1])
+        int minPrice=prices[0];
+        int currentProfit=0;
+        int maxProfit=0;
+        if(pricesSize<2)
         {
-            return prices[1]-prices[0];
+            return 0;
         }
-        else return 0;
-    }
-    else if(pricesSize<2)
-    {
-        return 0;
-    }
-    for(int i=1;i<pricesSize;i++ )
-    {
-        if(prices[i]<minPrice)
+        if (pricesSize==2)
         {
-            minPrice=prices[i];
+            if(prices[0]<prices[1])
+            {
+                return prices[1]-prices[0];
+            }
+            else return 0;
         }
-        currentProfit=prices[i]-minPrice;
-        if(currentProfit>maxProfit)
-        {
-            maxProfit=currentProfit;
-        }    
-            
-    }
-    return maxProfit;
         
-    }
+        for(int i=1;i<pricesSize;i++ )
+        {
+            if(prices[i]<minPrice)
+            {
+                minPrice=prices[i];
+            }
+            currentProfit=prices[i]-minPrice;
+            if(currentProfit>maxProfit)
+            {
+                maxProfit=currentProfit;
+            }    
+                
+        }
+        return maxProfit;
+            
+        }
 };
