078c449a97cc2caf01790cbd4de6a07fbedabec6
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-12-20 11:50:58 +0530

    Time: 92 ms (5.02%) | Memory: 19.5 MB (31.33%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -9,11 +9,12 @@ public:
         if(s==t)return 0;
 
         map<char,int> mp;
+
         for(char ch:s){
             if(mp.find(ch)==mp.end()) mp[ch]=1;
             else mp[ch]++;
         }
-        for(auto it:mp)cout<<it.first<<" "<<it.second<<endl;
+        
         int sum=0;
         for(auto x:mp){
             int temp=count(t.begin(),t.end(),x.first);
