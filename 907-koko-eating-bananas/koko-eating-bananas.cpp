6c5b7fe1ed13defd8e423cff077fff236b49cf50
Guhasri R <118996341+Guhasri@users.noreply.github.com>
2024-12-20 11:19:32 +0530

    Time: 3 ms (95.71%) | Memory: 22.9 MB (31.42%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int greatest = *max_element(piles.begin(), piles.end());  // Get the largest pile size
-        int smallest = 1;  // Minimum speed is 1 banana per hour
-        int ans = greatest;
-
-        while (smallest <= greatest) {
-            int mid = smallest + (greatest - smallest) / 2;
-           long long hours = 0;  // Use long long to handle large sums
-        for (int pile : piles) {
-            hours += (pile + mid - 1) / mid;  // This is equivalent to ceil(pile / k)
-        }
-
-            if (hours <= h) {
-                ans = mid;  // We can eat all piles within h hours with speed `mid`
-                greatest = mid - 1;  // Try a smaller speed
-            } else {
-                smallest = mid + 1;  // We need a larger speed to eat all piles in `h` hours
+        int i=1;
+        int j=*max_element(piles.begin(), piles.end()); 
+        int mins=j;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            long long sum=0;
+            for(int x:piles){
+                sum+=(x+mid-1)/mid;
+            }
+            if(sum<=h){
+                mins=min(mins,mid);
+                j=mid-1;
+            }
+            else {
+                i=mid+1;
             }
         }
-
-        return ans;
+        return mins;
     }
 };
